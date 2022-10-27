23d49c8d7440da986a4c50b5cf4680a29ecb6cb1
DESKTOP-LUT0RK2\ianni <iannilo01@gmail.com>
2022-10-26 23:04:26 -0300

    Regra de três adicionado

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,5 +1,7 @@
+import 'package:dart_playground/regra_de_tres.dart';
 import 'package:dart_playground/sequencia_fibonaci.dart';
 
 void main() {
-  sequenciaDeFibonaci();
+  //sequenciaDeFibonaci();
+  //regraDeTres(a: 10, b: 50, c: 20);
 }
---
+++ lib/regra_de_tres.dart
@@ -0,0 +1,15 @@
+void regraDeTres({double a = 0, double b = 0, double c = 0, double d = 0}) {
+  if (d == 0) {
+    d = (c * b) / a;
+    print('resultado: $d');
+  } else if (c == 0) {
+    c = (d * a) / b;
+    print('resultado: $c');
+  } else if (b == 0) {
+    b = (a * c) / d;
+    print('resultado: $b');
+  } else {
+    a = (c * b) / d;
+    print('resultado: $a');
+  }
+}
