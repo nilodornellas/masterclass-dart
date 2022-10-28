483032e07c43a9dcd23276bc114d36fea3cd926e
DESKTOP-LUT0RK2\ianni <iannilo01@gmail.com>
2022-10-27 22:49:20 -0300

    Adicionado a validação do cartão de crédito

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,7 +1,9 @@
 import 'package:dart_playground/regra_de_tres.dart';
 import 'package:dart_playground/sequencia_fibonaci.dart';
+import 'package:dart_playground/validacao_cartao.dart';
 
 void main() {
   //sequenciaDeFibonaci();
-  //regraDeTres(a: 10, b: 50, c: 20);
+  //regraDeTres(a: 10, b: 50, c: 20);\
+  validacaoCartaoDeCredito();
 }
