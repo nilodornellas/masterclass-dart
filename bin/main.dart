d94df8430bf93d987d3a48d42bece7455c1412ce
Nilo Dornellas <iannilo01@gmail.com>
2022-10-27 23:03:30 -0300

    Editado main do projeto

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,9 @@
+import 'package:dart_playground/week_1_dart_basics/sequencia_fibonaci.dart';
+import 'package:dart_playground/week_1_dart_basics/regra_de_tres.dart';
 import 'package:dart_playground/week_1_dar_basics/validacao_cartao.dart';
 
 void main() {
-  //sequenciaDeFibonaci();
-  //regraDeTres(a: 10, b: 50, c: 20);\
+  sequenciaDeFibonaci();
+  regraDeTres(a: 10, b: 50, c: 20);
   validacaoCartaoDeCredito();
 }
