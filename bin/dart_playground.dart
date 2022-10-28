baae45ba5e34b9046d5963cc9986355acc5a452a
DESKTOP-LUT0RK2\ianni <iannilo01@gmail.com>
2022-10-27 22:52:53 -0300

    Refatorado estrutura do projeto

--- bin/dart_playground.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dart_playground/regra_de_tres.dart';
-import 'package:dart_playground/sequencia_fibonaci.dart';
-import 'package:dart_playground/validacao_cartao.dart';
-
-void main() {
-  //sequenciaDeFibonaci();
-  //regraDeTres(a: 10, b: 50, c: 20);\
-  validacaoCartaoDeCredito();
-}
---
+++ bin/main.dart
@@ -0,0 +1,9 @@
+import 'package:dart_playground/week_1_dar_basics/regra_de_tres.dart';
+import 'package:dart_playground/week_1_dar_basics/sequencia_fibonaci.dart';
+import 'package:dart_playground/week_1_dar_basics/validacao_cartao.dart';
+
+void main() {
+  //sequenciaDeFibonaci();
+  //regraDeTres(a: 10, b: 50, c: 20);\
+  validacaoCartaoDeCredito();
+}
